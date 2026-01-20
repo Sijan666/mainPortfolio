30ada46f0bbd13537f27f94c61014e62a4549b44
Sijan666 <666majharulislam@gmail.com>
2026-01-20 19:27:50 +0600

    update in index css and hobbies

--- src/components/layouts/Hobbies.jsx
+++ src/components/layouts/Hobbies.jsx
@@ -21,133 +21,133 @@ const Hobbies = () => {
                 </div>
                 <div className="pt-[60px]">
                     <Flex className={'justify-between gap-x-7.5'}>
-                        <div className="py-3 pr-3 pl-3 w-70 bg-transparent rounded-lg shadow-newMade border border-slate-800 overflow-hidden group">
-                            <Flex className={'gap-x-5'}>
-                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300">
+                        <div className="py-3 pr-3 pl-3 w-[280px] bg-transparent rounded-lg border border-slate-800 group duration-300 shadow-newMade hover:shadow-gray-700">
+                            <div className="flex gap-x-5 items-center">
+                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300 flex justify-center items-center">
                                     <Monitor className='-rotate-45'/>
                                 </div>
                                 <div className="">
                                     <p className='text-white font-medium text-[17px] '>Developing</p>
                                 </div>
-                            </Flex>
+                            </div>
                         </div>
-                        <div className="py-3 pr-3 pl-3 w-70 bg-transparent rounded-lg shadow-newMade border border-slate-800 overflow-hidden group">
-                            <Flex className={'gap-x-5'}>
-                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300">
+                        <div className="py-3 pr-3 pl-3 w-[280px] bg-transparent rounded-lg border border-slate-800 group duration-300 shadow-newMade hover:shadow-gray-700">
+                            <div className="flex gap-x-5 items-center">
+                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300 flex justify-center items-center">
                                     <FaRegStopCircle className='-rotate-45 text-[25px]'/>
                                 </div>
                                 <div className="">
                                     <p className='text-white font-medium text-[17px]'>Mac OS</p>
                                 </div>
-                            </Flex>
+                            </div>
                         </div>
-                        <div className="py-3 pr-3 pl-3 w-70 bg-transparent rounded-lg shadow-newMade border border-slate-800 overflow-hidden group">
-                            <Flex className={'gap-x-5'}>
-                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300">
+                        <div className="py-3 pr-3 pl-3 w-[280px] bg-transparent rounded-lg border border-slate-800 group duration-300 shadow-newMade hover:shadow-gray-700">
+                            <div className="flex gap-x-5 items-center">
+                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300 flex justify-center items-center">
                                     <BsCameraVideo className='-rotate-45 text-[25px]'/>
                                 </div>
                                 <div className="">
                                     <p className='text-white font-medium text-[17px]'>Cinema</p>
                                 </div>
-                            </Flex>
+                            </div>
                         </div>
-                        <div className="py-3 pr-3 pl-3 w-70 bg-transparent rounded-lg shadow-newMade border border-slate-800 overflow-hidden group">
-                            <Flex className={'gap-x-5'}>
-                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300">
+                        <div className="py-3 pr-3 pl-3 w-[280px] bg-transparent rounded-lg border border-slate-800 group duration-300 shadow-newMade hover:shadow-gray-700">
+                            <div className="flex gap-x-5 items-center">
+                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300 flex justify-center items-center">
                                     <FiCoffee className='-rotate-45 text-[25px]'/>
                                 </div>
                                 <div className="">
                                     <p className='text-white font-medium text-[17px]'>Coffee</p>
                                 </div>
-                            </Flex>
+                            </div>
                         </div>
                     </Flex>
                 </div>
                 <div className="pt-7.5">
                     <Flex className={'justify-between gap-x-7.5'}>
-                        <div className="py-3 pr-3 pl-3 w-70 bg-transparent rounded-lg shadow-newMade border border-slate-800 overflow-hidden group">
-                            <Flex className={'gap-x-5'}>
-                                <div className="h-10 w-10 p-2.5 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300">
+                        <div className="py-3 pr-3 pl-3 w-[280px] bg-transparent rounded-lg border border-slate-800 group duration-300 shadow-newMade hover:shadow-gray-700">
+                            <div className="flex gap-x-5 items-center">
+                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300 flex justify-center items-center">
                                     <PiMusicNotesSimpleBold className='-rotate-45 text-[20px]'/>
                                 </div>
                                 <div className="">
                                     <p className='text-white font-medium text-[17px] '>Music</p>
                                 </div>
-                            </Flex>
+                            </div>
                         </div>
-                        <div className="py-3 pr-3 pl-3 w-70 bg-transparent rounded-lg shadow-newMade border border-slate-800 overflow-hidden group">
-                            <Flex className={'gap-x-5'}>
-                                <div className="h-10 w-10 p-2.5 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300">
+                        <div className="py-3 pr-3 pl-3 w-[280px] bg-transparent rounded-lg border border-slate-800 group duration-300 shadow-newMade hover:shadow-gray-700">
+                            <div className="flex gap-x-5 items-center">
+                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300 flex justify-center items-center">
                                     <PiWatchBold className='-rotate-45 text-[20px]'/>
                                 </div>
                                 <div className="">
                                     <p className='text-white font-medium text-[17px]'>Games</p>
                                 </div>
-                            </Flex>
+                            </div>
                         </div>
-                        <div className="py-3 pr-3 pl-3 w-70 bg-transparent rounded-lg shadow-newMade border border-slate-800 overflow-hidden group">
-                            <Flex className={'gap-x-5'}>
-                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300">
+                        <div className="py-3 pr-3 pl-3 w-[280px] bg-transparent rounded-lg border border-slate-800 group duration-300 shadow-newMade hover:shadow-gray-700">
+                            <div className="flex gap-x-5 items-center">
+                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300 flex justify-center items-center">
                                     <LiaCubeSolid className='-rotate-45 text-[25px]'/>
                                 </div>
                                 <div className="">
                                     <p className='text-white font-medium text-[17px]'>Designing</p>
                                 </div>
-                            </Flex>
+                            </div>
                         </div>
-                        <div className="py-3 pr-3 pl-3 w-70 bg-transparent rounded-lg shadow-newMade border border-slate-800 overflow-hidden group">
-                            <Flex className={'gap-x-5'}>
-                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300">
+                        <div className="py-3 pr-3 pl-3 w-[280px] bg-transparent rounded-lg border border-slate-800 group duration-300 shadow-newMade hover:shadow-gray-700">
+                            <div className="flex gap-x-5 items-center">
+                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300 flex justify-center items-center">
                                     <LuTarget className='-rotate-45 text-[25px]'/>
                                 </div>
                                 <div className="">
                                     <p className='text-white font-medium text-[17px]'>Sports</p>
                                 </div>
-                            </Flex>
+                            </div>
                         </div>
                     </Flex>
                 </div>
                 <div className="pt-7.5">
                     <Flex className={'justify-between gap-x-7.5'}>
-                        <div className="py-3 pr-3 pl-3 w-70 bg-transparent rounded-lg shadow-newMade border border-slate-800 overflow-hidden group">
-                            <Flex className={'gap-x-5'}>
-                                <div className="h-10 w-10 p-2.5 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300">
+                        <div className="py-3 pr-3 pl-3 w-[280px] bg-transparent rounded-lg border border-slate-800 group duration-300 shadow-newMade hover:shadow-gray-700">
+                            <div className="flex gap-x-5 items-center">
+                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300 flex justify-center items-center">
                                     <PiPenNibStraightBold className='rotate-90 text-[20px]'/>
                                 </div>
                                 <div className="">
                                     <p className='text-white font-medium text-[17px] '>Painting</p>
                                 </div>
-                            </Flex>
+                            </div>
                         </div>
-                        <div className="py-3 pr-3 pl-3 w-70 bg-transparent rounded-lg shadow-newMade border border-slate-800 overflow-hidden group">
-                            <Flex className={'gap-x-5'}>
-                                <div className="h-10 w-10 p-2.5 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300">
+                        <div className="py-3 pr-3 pl-3 w-[280px] bg-transparent rounded-lg border border-slate-800 group duration-300 shadow-newMade hover:shadow-gray-700">
+                            <div className="flex gap-x-5 items-center">
+                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300 flex justify-center items-center">
                                     <LuBook  className='-rotate-45 text-[20px]'/>
                                 </div>
                                 <div className="">
                                     <p className='text-white font-medium text-[17px]'>Reading</p>
                                 </div>
-                            </Flex>
+                            </div>
                         </div>
-                        <div className="py-3 pr-3 pl-3 w-70 bg-transparent rounded-lg shadow-newMade border border-slate-800 overflow-hidden group">
-                            <Flex className={'gap-x-5'}>
-                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300">
+                        <div className="py-3 pr-3 pl-3 w-[280px] bg-transparent rounded-lg border border-slate-800 group duration-300 shadow-newMade hover:shadow-gray-700">
+                            <div className="flex gap-x-5 items-center">
+                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300 flex justify-center items-center">
                                     <MdOutlinePhoneAndroid  className='-rotate-45 text-[25px]'/>
                                 </div>
                                 <div className="">
                                     <p className='text-white font-medium text-[17px]'>Android</p>
                                 </div>
-                            </Flex>
+                            </div>
                         </div>
-                        <div className="py-3 pr-3 pl-3 w-70 bg-transparent rounded-lg shadow-newMade border border-slate-800 overflow-hidden group">
-                            <Flex className={'gap-x-5'}>
-                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300">
-                                    <FiActivity  className='-rotate-45 text-[25px]'/>
+                        <div className="py-3 pr-3 pl-3 w-[280px] bg-transparent rounded-lg border border-slate-800 group duration-300 shadow-newMade hover:shadow-gray-700">
+                            <div className="flex gap-x-5 items-center">
+                                <div className="h-10 w-10 p-2 rounded-xl rotate-45 bg-[#FE9A00]/10 text-[#FE9A00] group-hover:bg-[#FE9A00] group-hover:text-white duration-300 flex justify-center items-center">
+                                    <FiActivity className='-rotate-45 text-[25px]'/>
                                 </div>
                                 <div className="">
                                     <p className='text-white font-medium text-[17px]'>Other Activity</p>
                                 </div>
-                            </Flex>
+                            </div>
                         </div>
                     </Flex>
                 </div>
