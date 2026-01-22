f89a4f72cb8bbc29a90f77fc4636bb9a216a9cd7
Sijan666 <666majharulislam@gmail.com>
2026-01-22 20:31:51 +0600

    add autoplay in slider

--- src/components/layouts/Reviews.jsx
+++ src/components/layouts/Reviews.jsx
@@ -18,14 +18,15 @@ var settings = {
     infinite: true,
     speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 4,
+    slidesToScroll: 1,
     arrows : false ,
+    autoplay : true ,
     responsive: [
         {
             breakpoint: 1024,
             settings: {
             slidesToShow: 3,
-            slidesToScroll: 3,
+            slidesToScroll: 1,
             infinite: true,
             dots: false
             }
